dced7456fed8781ec2cbf47148f338c5cd938b60
mellisa <mellisamugiyo1@gmail.com>
2023-06-14 08:11:24 +0200

    adding requirements.txt and mybandblog/views

--- mybandblog/views.py
+++ mybandblog/views.py
@@ -1,12 +1,49 @@
+
+
 from django.shortcuts import render
 
 def home(request):
-   return render(request,'home.html')
+    """Constructor method, This method will be used to ask request 
+   for the  html page
+
+   :param request: request objects are used  to pass state through the system
+   since the html page has been requested, Django creates an HttpRequest object that
+    contains metadata about the request
+
+    :returns: It renders the html page(home.html)
+    :rtype: html pages
+
+"""
+    return render(request,'home.html')
 
 
 def information(request):
+    """Constructor method, This method will be used to ask request 
+   for the  html page
+
+   :param request: request objects are used  to pass state through the system
+   since the html page has been requested, Django creates an HttpRequest object that
+    contains metadata about the request
+
+    :returns: It renders the html page(information.html)
+    :rtype: html pages
+
+"""
     return render(request,'information.html',{})
 
 def contact(request):
+    """Constructor method, This method will be used to ask request 
+   for the  html page
+
+   :param request: request objects are used  to pass state through the system
+   since the html page has been requested, Django creates an HttpRequest object that
+    contains metadata about the request
+
+    :returns: It renders the html page(contact.html)
+    :rtype: html pages
+
+"""
     return render(request,'contact.html',{})
+
+
 
